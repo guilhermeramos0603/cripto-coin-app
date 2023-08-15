ac765910555b73e6a332277262d3f6c2a56a8714
Guilherme Ramos <guilherme.silvano@hotmail.com>
2023-08-15 20:38:30 -0300

    selected items

--- lib/presenter/coin/CoinPage.dart
+++ lib/presenter/coin/CoinPage.dart
@@ -1,27 +1,87 @@
 import 'package:cripto_coin/business/repositoriesImp/coin/CoinRepositoryImp.dart';
-import 'package:flutter/material.dart';
+import 'package:cripto_coin/core/models/coin/Coin.dart';
 
-class CoinPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CoinPage extends StatefulWidget {
   const CoinPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final table = CoinRepositoryImp.table;
-    return Scaffold(
-      appBar: AppBar(
+  State<CoinPage> createState() => _CoinPageState();
+}
+
+class _CoinPageState extends State<CoinPage> {
+  final table = CoinRepositoryImp.table;
+  NumberFormat dollar = NumberFormat.currency(locale: 'en-US', name: '\$');
+  List<Coin> selectedTable = [];
+
+  appBarDynamic() {
+    if (selectedTable.isEmpty) {
+      return AppBar(
         title: const Text("Cripto Coin"),
+      );
+    }
+
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () {
+          setState(() {
+            selectedTable = [];
+          });
+        },
       ),
+      iconTheme: const IconThemeData(color: Colors.black54),
+      title: Text('${selectedTable.length} selected'),
+      titleTextStyle: const TextStyle(
+          color: Colors.black54, fontSize: 20, fontWeight: FontWeight.bold),
+      backgroundColor: Colors.blueGrey.shade50,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarDynamic(),
       body: ListView.separated(
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-              leading: Image.asset(table[index].icon),
-              title: Text(table[index].name),
-              trailing: Text(table[index].price.toString()),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              leading: (selectedTable.contains(table[index]))
+                  ? const CircleAvatar(
+                      child: Icon(Icons.check),
+                    )
+                  : SizedBox(
+                      child: Image.asset(
+                      table[index].icon,
+                      width: 30,
+                    )),
+              title: Text(
+                table[index].name,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+              trailing: Text(dollar.format(table[index].price)),
+              selected: selectedTable.contains(table[index]),
+              selectedTileColor: Colors.red.shade100,
+              onTap: () {
+                setState(() {
+                  (selectedTable.contains(table[index]))
+                      ? selectedTable.remove(table[index])
+                      : selectedTable.add(table[index]);
+                });
+              },
             );
           },
           padding: const EdgeInsets.all(16),
           separatorBuilder: (_, __) => const Divider(),
           itemCount: table.length),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {}, label: Text('Favorite')),
     );
   }
 }
